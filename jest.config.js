476f89855b4183a137706a05ead98d0003f187dd
Miguel <miguel.danto@gmail.com>
2022-10-26 21:38:07 -0500

    agregando configuracion

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
-export default{
+/** @type {import('jest').Config} */
+export default {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'jsdom',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFiles:[
+      "jest-canvas-mock"
+    ]
   };
