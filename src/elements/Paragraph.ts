bf87a35cd6f09dbd45d12f435aac458ad4314e3f
Miguel <miguel.danto@gmail.com>
2023-01-10 17:48:29 -0600

    fix bug

--- src/elements/Paragraph.ts
+++ src/elements/Paragraph.ts
@@ -135,7 +135,7 @@ export default class Paragraph extends ElementInPage {
         }
 
 
-        this.sendPositionateEventToSuscribers()
+        this.sendPositionateEventToSuscribers(false)
     }
 
     private tryDistributeNextSpace(
